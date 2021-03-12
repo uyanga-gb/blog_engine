a654b20f950151e815b144143d07ca3ba1e2fc82
uyanga-gb <uyangaganbaatar@MacBook-Pro-4.local>
2021-03-11 17:19:44 -0800

    pushing left out file changes

--- node-postgres/blog_model.js
+++ node-postgres/blog_model.js
@@ -10,9 +10,9 @@ const pool = new Pool({
   port: 5432,
 });
 
-const getUsers = () => {
+const getPosts = () => {
   return new Promise(function (resolve, reject) {
-    pool.query('SELECT * FROM blog_user ORDER BY id ASC', (error, results) => {
+    pool.query('SELECT post.id as postId, post.title, post.postcontent, post.publishedat, blog_user.firstname as authorFirstName, blog_user.lastname as authorLastName FROM post, blog_user WHERE post.authorid = blog_user.id ORDER BY post.publishedat ASC', (error, results) => {
       if (error) {
         reject(error)
       }
@@ -32,11 +32,23 @@ const getUserByLogin = (user) => {
   })
 }
 
+const getUser = (username) => {
+  return new Promise(function (resolve, reject) {
+    pool.query('SELECT * FROM blog_user WHERE email = $1', [username], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      console.log('result ', results)
+      return resolve(results.rows);
+    })
+  })
+}
+
 const addNewUser = (newUser) => {
   return new Promise(function (resolve, reject) {
-    pool.query('INSERT INTO blog_user (firstname, lastname, email, passwordhash, createdat, updatedat, lastlogin, intro, profile) ' +
-      'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-      [newUser.firstname, newUser.lastname, newUser.username, newUser.password, new Date(), new Date(), new Date(), newUser.intro, 'New Profile'], (error, results) => {
+    pool.query('INSERT INTO blog_user (firstname, lastname, email, passwordhash, intro, createdat, updatedat) ' +
+      'VALUES ($1, $2, $3, $4, $5, $6, $7)',
+      [newUser.firstname, newUser.lastname, newUser.username, newUser.password, newUser.intro, new Date(), new Date()], (error, results) => {
       if (error) {
         reject(error)
       } else if (results) {
@@ -46,8 +58,50 @@ const addNewUser = (newUser) => {
   })
 }
 
+const addNewPost = (newPost) => {
+  return new Promise(function (resolve, reject) {
+    pool.query('INSERT INTO post (authorid, title, postcontent, createdat, publishedat) ' +
+      'VALUES ($1, $2, $3, $4, $5)',
+      [newPost.authorId, newPost.title, newPost.content, new Date(), new Date()], (error, results) => {
+        if (error) {
+          reject(error)
+        } else if (results) {
+          resolve(results.rows);
+        }
+      })
+  })
+}
+
+const addNewPostComment = (newPostComment) => {
+  return new Promise(function (resolve, reject) {
+    pool.query('INSERT INTO post_comment (postid, title, postcomment, createdat, publishedat) ' +
+      'VALUES ($1, $2, $3, $4, $5)',
+      [newPostComment.postId, newPostComment.title, newPostComment.postComment, new Date(), new Date()], (error, results) => {
+        if (error) {
+          reject(error)
+        } else if (results) {
+          resolve(results.rows);
+        }
+      })
+  })
+}
+
+const getPost = (postId) => {
+  return new Promise(function (resolve, reject) {
+    pool.query('SELECT * FROM post WHERE id = $1', [postId], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  })
+}
+
 module.exports = {
-  getUsers,
+  getPosts,
   addNewUser,
-  getUserByLogin
+  getUserByLogin,
+  getUser,
+  addNewPost,
+  addNewPostComment
 }
--- node-postgres/index.js
+++ node-postgres/index.js
@@ -22,33 +22,69 @@ app.use(express.json());
 // CORS implemented so that we don't get errors when trying to access the server from a different server location
 app.use(cors());
 
-app.get('/', (req, res) => {
-  blog_model.getUsers()
-    .then((data) => {
-      console.log(data);
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
+// User requests
 app.post('/register', (req, res) => {
   blog_model.addNewUser(req.body)
-    .then(() => {
-      console.log('new user created');
-      return res.json({msg: "User added"});
+    .then((data) => {
+      console.log('new user created', data);
+      return res.json(data);
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-app.get('/userlogin', (req, res) => {
+app.post('/userlogin', (req, res) => {
   blog_model.getUserByLogin(req.body)
     .then((data) => {
       console.log('login ', data);
       res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  const email = req.params.id
+  blog_model.getUser(email)
+    .then((data) => {
+      console.log('get user ',data);
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Post requests
+app.get('/posts', (req, res) => {
+  blog_model.getPosts()
+    .then((data) => {
+      console.log('all posts', data);
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/addPost', (req, res) => {
+  blog_model.addNewPost(req.body)
+    .then(() => {
+      console.log('new post created');
+      return res.json({msg: "Post added"});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/addPostComment', (req, res) => {
+  blog_model.addNewPostComment(req.body)
+    .then(() => {
+      console.log('new post comment added');
+      return res.json({msg: "Post comment added"});
     })
     .catch((err) => {
       console.log(err);
